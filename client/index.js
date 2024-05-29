d783f52cf62bf553850796006ef6aec14ea806c1
rajiknows <rajeshhjhamain@gmail.com>
2024-05-29 19:48:05 +0530

    first push all task done

--- client/index.js
+++ client/index.js
@@ -1,14 +1,23 @@
 const axios = require('axios');
 const niceList = require('../utils/niceList.json');
 const MerkleTree = require('../utils/MerkleTree');
+const verifyProof = require('../utils/verifyProof');
 
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
 
+  const name  = "Bill Toy";
+  const merkletree = new MerkleTree(niceList);
+  const proof = merkletree.getProof(niceList.indexOf("Bill Toy"))
+  const verify = verifyProof(proof,name,merkletree.getRoot());
+  if(verifyProof){
+    console.log("proved")
+  }
+
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    name: name
   });
 
   console.log({ gift });
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const verifyProof = require('../utils/verifyProof');
-
+const MerkleTree = require('../utils/MerkleTree')
+const niceList = require('../utils/niceList.json')
 const port = 1225;
 
 const app = express();
@@ -8,19 +9,24 @@ app.use(express.json());
 
 // TODO: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+let mtree = new MerkleTree(niceList)
+const MERKLE_ROOT = mtree.getRoot();
 
 app.post('/gift', (req, res) => {
   // grab the parameters from the front-end here
-  const body = req.body;
+  const {name} = req.body;
 
   // TODO: prove that a name is in the list 
-  const isInTheList = false;
+  const proof = mtree.getProof(niceList.indexOf(name))
+
+  console.log(name)
+  
+  const isInTheList = verifyProof(proof,name,mtree.getRoot()) ? true : false 
   if(isInTheList) {
     res.send("You got a toy robot!");
   }
   else {
-    res.send("You are not on the list :(");
+    res.send(`You are not on the list :( ${name}` );
   }
 });
 
